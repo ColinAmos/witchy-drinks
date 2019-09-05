a14cafbf5f863eb90f2e0695c95a461ebabc4f25
Colin Amos <35937916+ColinAmos@users.noreply.github.com>
2019-09-05 13:59:02 -0400

    Replace in cauldron effect with glow
    using css filter: drop-shadow()

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -2,17 +2,14 @@ import React from "react";
 
 import Draggable from "react-draggable";
 
-
 const Ingredient = ({ image, x, y, index, inCauldron, scale, handleDragStop, handleDragMove }) => {
-    let effectStyle = { display: "none" }
-    if (inCauldron) {
-        effectStyle.display = "block";
-    }
+    let className = "ingredient";
+    if (inCauldron) className += " glowing";
+    
     return (
         <Draggable scale={scale} onStop={(e) => handleDragStop(e, index)} onDrag={(e)=>handleDragMove(e, index)}>
             <div className="ingredient-wrapper" style={{ left: x, top: y }}>
-                <img alt="" src={image} id={"ingredient" + index} className={"ingredient"} draggable="false"/>
-                <div className="ingredient--in-cauldron-effect" style={effectStyle}/>
+                <img alt="" src={image} id={"ingredient" + index} className={className} draggable="false"/>
             </div>
         </Draggable>
     )
