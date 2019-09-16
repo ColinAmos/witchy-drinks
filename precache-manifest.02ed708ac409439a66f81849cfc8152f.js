b2416a54aa58da1bdc3c2f9fb39c53ef8d3c3076
Colin Amos <35937916+ColinAmos@users.noreply.github.com>
2019-09-16 16:39:56 -0400

    Updates

---
+++ precache-manifest.02ed708ac409439a66f81849cfc8152f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a04647062ba223d7ab598d0ede3a19",
+    "url": "/witchy-drinks/index.html"
+  },
+  {
+    "revision": "adcce236a3c6607bfc24",
+    "url": "/witchy-drinks/static/css/main.e03afa68.chunk.css"
+  },
+  {
+    "revision": "fae5bb13ed04e454c2bc",
+    "url": "/witchy-drinks/static/js/2.b1cda37a.chunk.js"
+  },
+  {
+    "revision": "adcce236a3c6607bfc24",
+    "url": "/witchy-drinks/static/js/main.03f2284f.chunk.js"
+  },
+  {
+    "revision": "426f173abde95159a2bb",
+    "url": "/witchy-drinks/static/js/runtime~main.1e3ba57a.js"
+  },
+  {
+    "revision": "3ec6894471dbeb700bb635149614e0de",
+    "url": "/witchy-drinks/static/media/cauldron.3ec68944.png"
+  },
+  {
+    "revision": "defe5c6056e42b492b659430d4b576e6",
+    "url": "/witchy-drinks/static/media/placeholder-background-image.defe5c60.jpg"
+  },
+  {
+    "revision": "eae7f448597277cbb645329a2b0beb43",
+    "url": "/witchy-drinks/static/media/potion-mask.eae7f448.png"
+  },
+  {
+    "revision": "3ee3aa2410781e35017ca74d38dfc112",
+    "url": "/witchy-drinks/static/media/potion__blue.3ee3aa24.png"
+  },
+  {
+    "revision": "11bf51e32aa3568f8dbb58a711711948",
+    "url": "/witchy-drinks/static/media/potion__brown.11bf51e3.png"
+  },
+  {
+    "revision": "3d3c66bcb870b88d2bddb974b2991a54",
+    "url": "/witchy-drinks/static/media/potion__cyan.3d3c66bc.png"
+  },
+  {
+    "revision": "2b30d28ab27cf32229bd16092284c0b2",
+    "url": "/witchy-drinks/static/media/potion__gray.2b30d28a.png"
+  },
+  {
+    "revision": "b8228b60fdf04c08c44684f222d1335a",
+    "url": "/witchy-drinks/static/media/potion__green.b8228b60.png"
+  },
+  {
+    "revision": "f505d1fae3bf23ff182a4f2c094fa003",
+    "url": "/witchy-drinks/static/media/potion__orange.f505d1fa.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8c9cc60f214d50f038069387d5cebb07.js"
+  "/witchy-drinks/precache-manifest.02ed708ac409439a66f81849cfc8152f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/witchy-drinks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
